a76fc7af8308001bdda2f57da2b3c4fbc25c67d7
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2017-07-11 23:27:09 +0200

    lorem ipsum example

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main() {
     uint8_t *str = (uint8_t *)"a utf8 string";
-    uint8_t *str2 = (uint8_t *)"another utf8 string ©";
+    uint8_t *str2 = (uint8_t *)"Ḽơᶉëᶆ ȋṕšᶙṁ ḍỡḽǭᵳ ʂǐť ӓṁệẗ, ĉṓɲṩḙċťᶒțûɾ ấɖḯƥĭṩčįɳġ ḝłįʈ, șếᶑ ᶁⱺ ẽḭŭŝḿꝋď ṫĕᶆᶈṓɍ ỉñḉīḑȋᵭṵńť ṷŧ ḹẩḇőꝛế éȶ đꝍꞎôꝛȇ ᵯáꞡᶇā ąⱡîɋṹẵ.";
 
     utf8_t *ustr = convert_to_utf8(str, strlen((char *)str) + 1);
     utf8_t *ustr2 = convert_to_utf8(str2, strlen((char *)str2) + 1);
