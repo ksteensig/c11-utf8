701fcea1ddac38faa3dd5e47937d3c652191bdea
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2017-07-11 20:27:38 +0200

    refactored and added some better utf8 representation and parsing

--- include/utf8.h
+++ include/utf8.h
@@ -2,41 +2,76 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+#define DEFAULT_UTF8_STRING_LENGTH 4
 
 #define FOUR_BYTE_HEAD 0b11110
-#define FOUR_BYTE(c) ((c >> 27) ^ FOUR_BYTE_HEAD)
+#define FOUR_BYTE(c) (((c >> 3) ^ FOUR_BYTE_HEAD) == 0)
 
 #define THREE_BYTE_HEAD 0b1110
-#define THREE_BYTE(c) (((c >> 20) & 0xF) ^ THREE_BYTE_HEAD)
+#define THREE_BYTE(c) (((c >> 4) ^ THREE_BYTE_HEAD) == 0)
 
 #define TWO_BYTE_HEAD 0b110
-#define TWO_BYTE(c) (((c >> 13) & 0x7) ^ TWO_BYTE_HEAD)
+#define TWO_BYTE(c) (((c >> 5) ^ TWO_BYTE_HEAD) == 0)
 
 #define ONE_BYTE_HEAD 0b0
-#define ONE_BYTE(c) (((0b0 >> 7) & 0x0) ^ ONE_BYTE_HEAD)
-#define FOLLOW_BYTE_HEAD(c, s) (((c >> s) & 0xFF) ^ 0b10)
-#define ZERO(c, s) (c >> s)
+#define ONE_BYTE(c) (((c >> 7) & ONE_BYTE_HEAD) == 0)
 
-bool is_1byte(uint32_t ch);
-bool is_2byte(uint32_t ch);
-bool is_3byte(uint32_t ch);
-bool is_4byte(uint32_t ch);
+#define TRAILING_BYTE_HEAD 0b10
+#define TRAILING_BYTE(c) (((c >> 6) ^ 0b10) == 0)
 
-typedef struct utf8str_s {
-    uint8_t *utf8_string;
-    uint64_t arr_len;
-    uint64_t chars;
-    uint64_t last_inserted_pos;
-    uint8_t last_inserted_length;
-} utf8str_t;
+bool is_1byte(uint8_t b);
+bool is_2byte(uint8_t b);
+bool is_3byte(uint8_t b);
+bool is_4byte(uint8_t b);
+bool is_trailing(uint8_t b);
 
+typedef enum utf8_error_e {
+    INVALID_UTF8        = 0,
+    OUT_OF_BOUND_ERROR  = 1,
+    NULL_ERROR          = 2,
+    UNKNOWN             = 0xFF
+} utf8_error_t;
+
+// cache for storing latest insertion position and length of character inserted in an utf8 string
+typedef struct utf8_cache_s {
+    uint64_t inserted_pos;  // position of first byte
+    uint8_t inserted_length;// length of inserted utf8 character
+} utf8_cache_t;
+
+// utf8 representation
+typedef struct utf8rep_s {
+    uint8_t *utf8_string;   // the utf8 string represented by a uint8_t array
+    uint64_t bytes;         // utf8_string array length
+    uint64_t chars;         // char count
+    utf8_cache_t cache;     // contains where the last char was inserted and how many uint8_t it is
+} utf8rep_t;
+
+// wrapper type which can be an error or a utf8 string
 typedef struct utf8_s {
-    uint8_t bytes;
-    uint8_t uchar[4];
+    bool is_error; // false means it's a utf8rep_t
+    utf8_error_t error;
+    utf8rep_t *u8str;
 } utf8_t;
 
-utf8_t convert_to_utf8(uint32_t ch);
+utf8_t *init_utf8();
+void free_utf8rep(utf8_t *ustr);
+void free_utf8(utf8_t *ustr);
 
-utf8str_t utf8_concat(utf8str_t ustr1, utf8str_t ustr2);
-utf8str_t utf8_append(utf8str_t ustr, utf8_t uchar);
-utf8str_t utf8_prepend(utf8str_t ustr, utf8_t uchar);
+utf8_t *grow_string(utf8_t *ustr);
+utf8_t *convert_to_utf8(uint8_t *bytes, uint8_t *end_of_array);
+void add_character(utf8_t *ustr, uint8_t bytes[4], uint8_t len);
+
+/*
+utf8_t *utf8_concat(utf8_t ustr1, utf8_t ustr2);
+utf8_t *utf8_append(utf8_t ustr1, utf8_t ustr2);
+utf8_t *utf8_prepend(utf8_t ustr1, utf8_t ustr2);
+utf8_t *utf8_insert(utf8_t ustr, _utf8_t uchar, uint64_t pos);
+utf8_t *utf8_remove(utf8_t ustr, uint64_t pos);
+utf8_t *utf8_overwrite(utf8_t ustr, uint64_t pos);
+*/
+void utf8_print(utf8_t ustr);
+utf8_t utf8_read();
--- src/utf8.c
+++ src/utf8.c
@@ -1,32 +1,147 @@
 #include "utf8.h"
 
-bool is_1byte(uint32_t ch) {
-    return (ONE_BYTE(ch) == 0) &&
-           (ZERO(ch, 24) == 0) &&
-           (ZERO(ch, 16) == 0) &&
-           (ZERO(ch, 8) == 0);
+bool is_1byte(uint8_t b) {
+    return ONE_BYTE(b);
 }
 
-bool is_2byte(uint32_t ch) {
-    return (TWO_BYTE(ch) == 0) &&
-           (ZERO(ch, 24) == 0) &&
-           (ZERO(ch, 16) == 0) &&
-           (FOLLOW_BYTE_HEAD(ch, 0) == 0);
+bool is_2byte(uint8_t b) {
+    return TWO_BYTE(b);
 }
 
-bool is_3byte(uint32_t ch) {
-    return (THREE_BYTE(ch) == 0) &&
-           (ZERO(ch, 24) == 0) &&
-           (FOLLOW_BYTE_HEAD(ch, 8) == 0) &&
-           (FOLLOW_BYTE_HEAD(ch, 0) == 0);
+bool is_3byte(uint8_t b) {
+    return THREE_BYTE(b);
 }
 
-bool is_4byte(uint32_t ch) {
-    return (FOUR_BYTE(ch) == 0) &&
-           (FOLLOW_BYTE_HEAD(ch, 16) == 0) &&
-           (FOLLOW_BYTE_HEAD(ch, 8) == 0) &&
-           (FOLLOW_BYTE_HEAD(ch, 0) == 0);
+bool is_4byte(uint8_t b) {
+    return FOUR_BYTE(b);
 }
+
+bool is_trailing(uint8_t b) {
+    return TRAILING_BYTE(b);
+}
+
+utf8_t *init_utf8() {
+    utf8_t *ustr = malloc(sizeof(utf8_t));
+    utf8rep_t *ustr_rep = malloc(sizeof(utf8rep_t));
+    utf8_cache_t ustr_cache;
+    ustr_cache.inserted_length = 0;
+    ustr_cache.inserted_pos = 0;
+
+    ustr_rep->utf8_string = malloc(sizeof(uint8_t) * DEFAULT_UTF8_STRING_LENGTH);
+    ustr_rep->bytes = 0;
+    ustr_rep->chars = 0;
+    ustr_rep->cache = ustr_cache;
+
+    ustr->u8str = ustr_rep;
+    ustr->error = false;
+
+    return ustr;
+}
+
+void append_bytes(utf8_t *ustr, uint8_t bytes[4], uint8_t len) {
+    if (ustr->is_error) {
+        return;
+    }
+
+    switch (len) {
+        case 1:
+            (ustr->u8str)->utf8_string[ustr->u8str->bytes] = bytes[0];
+            ustr->u8str->bytes += len;
+            break;
+        case 2:
+            ustr->u8str->utf8_string[ustr->u8str->bytes] = bytes[1];
+            ustr->u8str->utf8_string[ustr->u8str->bytes + 1] = bytes[0];
+            ustr->u8str->bytes += len;
+            break;
+        case 3:
+            ustr->u8str->utf8_string[ustr->u8str->bytes] = bytes[2];
+            ustr->u8str->utf8_string[ustr->u8str->bytes + 1] = bytes[1];
+            ustr->u8str->utf8_string[ustr->u8str->bytes + 2] = bytes[0];
+            ustr->u8str->bytes += len;
+            break;
+        case 4:
+            ustr->u8str->utf8_string[ustr->u8str->bytes] = bytes[3];
+            ustr->u8str->utf8_string[ustr->u8str->bytes + 1] = bytes[2];
+            ustr->u8str->utf8_string[ustr->u8str->bytes + 2] = bytes[1];
+            ustr->u8str->utf8_string[ustr->u8str->bytes + 3] = bytes[0];
+            ustr->u8str->bytes += len;
+            break;
+        default:
+            ustr->is_error = true;
+            ustr->error = INVALID_UTF8;
+            return;
+    }
+
+    ustr->u8str->chars++;
+}
+
+utf8_t *convert_to_utf8(uint8_t *bytes, uint8_t *end_of_array) {
+    utf8_t *ustr = init_utf8();
+
+    if (bytes == NULL || end_of_array == NULL) {
+        ustr->is_error = true;
+        ustr->error = NULL_ERROR;
+        return ustr;
+    }
+
+    ptrdiff_t diff = end_of_array - bytes;
+    uint64_t counter = 0;
+
+    while ((bytes[counter] != '\0' || bytes[counter] != -1) && (counter < diff)) {
+        if (is_1byte(bytes[counter])) {
+            append_bytes(ustr, bytes, 1);
+            counter++;
+        } else if (is_2byte(bytes[counter] &&
+                   is_trailing(counter + 1))) {
+            append_bytes(ustr, bytes, 2);
+            counter += 2;
+        } else if (is_3byte(bytes[counter]  &&
+                   is_trailing(counter + 1) &&
+                   is_trailing(counter + 2))) {
+            append_bytes(ustr, bytes, 3);
+            counter += 3;
+        } else if (is_4byte(bytes[counter]  &&
+                   is_trailing(counter + 1) &&
+                   is_trailing(counter + 2) &&
+                   is_trailing(counter + 3))) {
+            append_bytes(ustr, bytes, 4);
+            counter += 4;
+        } else {
+            ustr->is_error = true;
+            ustr->error = INVALID_UTF8;
+        }
+
+        if (ustr->is_error) {
+            return ustr;
+        }
+
+        counter++;
+    }
+
+    return ustr;
+}
+
+/*
+utf8_t *grow_string(utf8_t *ustr) {
+    // double the possible string length
+    utf8_t *n_ustr = malloc(sizeof(utf8_t));
+    
+    n_ustr->utf8_string = malloc(sizeof(uint8_t) * ustr->bytes * 2);
+    n_ustr->bytes = ustr->bytes;
+    n_ustr->chars = ustr->chars;
+    n_ustr->_last_inserted_length = ustr->_last_inserted_length;
+    n_ustr->_last_inserted_pos = ustr->_last_inserted_pos;
+
+    memcpy(n_ustr->utf8_string, ustr->utf8_string, sizeof(uint8_t));
+
+    free(ustr->utf8_string);
+    free(ustr);
+
+    return n_ustr;
+}
+*/
+
+/*
 
 utf8_t convert_to_utf8(uint32_t ch) {
     utf8_t u;
@@ -55,3 +170,5 @@ utf8_t convert_to_utf8(uint32_t ch) {
 
     return u;
 }
+
+*/
